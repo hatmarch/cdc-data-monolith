8810bf959f0915cca8e2ffaed0e6da467447a586
Marc Hildenbrand <mwhildenbrand@gmail.com>
2021-02-27 06:07:56 +0000

    Add initial demo creation functionality
    create-demo.sh can be used to create the demo
    
    Uses an ansible playbook behind the scenes that can be found in the ansible/demo folder

---
+++ scripts/create-demo.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
+declare PROJECT_PREFIX="cdc-demo"
+declare -r ANSIBLE_DEMO_PLAYBOOK_DIR="${SCRIPT_DIR}/../ansible/demo"
+
+display_usage() {
+cat << EOF
+$0: Create CDC Monolith Demo --
+
+  Usage: ${0##*/} [ OPTIONS ]
+  
+    -p <TEXT>  [optional] Project prefix to use.  Defaults to "cdc-demo"
+    -v         [optional] Run ansible playbooks in verbose mode
+
+EOF
+}
+
+get_and_validate_options() {
+  # Transform long options to short ones
+#   for arg in "$@"; do
+#     shift
+#     case "$arg" in
+#       "--long-x") set -- "$@" "-x" ;;
+#       "--long-y") set -- "$@" "-y" ;;
+#       *)        set -- "$@" "$arg"
+#     esac
+#   done
+
+  
+  # parse options
+  while getopts ':vp:h' option; do
+      case "${option}" in
+          p  ) p_flag=true; PROJECT_PREFIX="${OPTARG}";;
+          v  ) ANSIBLE_VERBOSE="-vvvvv";;
+          h  ) display_usage; exit;;
+          \? ) printf "%s\n\n" "  Invalid option: -${OPTARG}" >&2; display_usage >&2; exit 1;;
+          :  ) printf "%s\n\n%s\n\n\n" "  Option -${OPTARG} requires an argument." >&2; display_usage >&2; exit 1;;
+      esac
+  done
+  shift "$((OPTIND - 1))"
+
+  if [[ -z "${PROJECT_PREFIX}" ]]; then
+      printf '%s\n\n' 'ERROR - PROJECT_PREFIX must not be null' >&2
+      display_usage >&2
+      exit 1
+  fi
+}
+
+main() {
+    # import common functions
+    . $SCRIPT_DIR/common-func.sh
+
+    trap 'error' ERR
+    trap 'cleanup' EXIT SIGTERM
+    trap 'interrupt' SIGINT
+
+    get_and_validate_options "$@"
+
+    #create the dev project if it doesn't already exist
+    dev_prj="${PROJECT_PREFIX}-dev"
+    oc get ns $dev_prj 2>/dev/null  || { 
+        oc new-project $dev_prj
+    }
+
+    # Run the ansible playbook (see inventory file for additional variables that can be overriden with -e)
+    echo "Attempting to run the ansible playbook for demo"
+    ansible-playbook -i ${ANSIBLE_DEMO_PLAYBOOK_DIR}/inventory ${ANSIBLE_DEMO_PLAYBOOK_DIR}/main.yaml \
+      -e "ACTION=create" \
+      -e "dev_project=${dev_prj}" $(echo ${ANSIBLE_VERBOSE:-""})
+
+    echo "Demo installation completed without error."
+}
+
+main "$@"
