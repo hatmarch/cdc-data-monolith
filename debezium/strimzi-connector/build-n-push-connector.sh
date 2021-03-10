1dd0cefd9df9f2de436409e7e18a1fce07c94708
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-10 02:42:12 +0000

    Add support for building a strimzi managed kafkaconnect with Debezium support

---
+++ debezium/strimzi-connector/build-n-push-connector.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
+
+declare IMAGE_TAG=${1:-latest}
+declare IMAGE_NAME=${2:-cdc-demo-dbz-connect}
+declare DOCKERFILE=${3:-Dockerfile}
+declare REGISTRY=${4:-"quay.io"}
+declare ACCOUNT=${5:-"mhildenb"}
+
+DOCKER_BUILDKIT=1 docker build --progress=plain --secret id=myuser,src=../../secrets/myuser.txt --secret id=mypass,src=../../secrets/mypass.txt -f ${DOCKERFILE} -t ${REGISTRY}/${ACCOUNT}/${IMAGE_NAME}:$IMAGE_TAG $SCRIPT_DIR
+
+docker tag ${REGISTRY}/${ACCOUNT}/${IMAGE_NAME}:${IMAGE_TAG} ${REGISTRY}/${ACCOUNT}/${IMAGE_NAME}:latest
+
+docker push ${REGISTRY}/${ACCOUNT}/${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${REGISTRY}/${ACCOUNT}/${IMAGE_NAME}:latest
